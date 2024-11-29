8e6db9be5128ed7c20b3f4fe8914fc45f9d556f1
riris03 <00001116841885sp@al.educacao.sp.gov.br>
2024-11-29 08:53:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Quem foi Dom Pedro I?',
+    'Dom Pedro I foi o primeiro imperador do Brasil.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o maior planeta do sistema solar?',
+    'O maior planeta do sistema solar é Júpiter.'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual filme ganhou o Oscar de Melhor Filme em 1998?',
+    'O filme "Titanic" ganhou o Oscar de Melhor Filme em 1998.'
+)
+
+criaCartao(
+    'Arte',
+    'Quem pintou a Mona Lisa?',
+    'A Mona Lisa foi pintada por Leonardo da Vinci.'
+)
